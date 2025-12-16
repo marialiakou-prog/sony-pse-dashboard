643db2efe52abb7e5296072bfd98f9e113e7a186
marialiakou-prog <maria.liakou@baresquare.com>
2025-12-16 18:53:10 +0200

    Increase countries list height to show 7 rows fully visible

--- app/page.tsx
+++ app/page.tsx
@@ -3238,7 +3238,7 @@ export default function Home() {
                     </div>
 
                     {/* List of countries */}
-                    <div className="flex-1 max-h-32 overflow-y-auto space-y-1">
+                    <div className="flex-1 max-h-56 overflow-y-auto space-y-1">
                       {llmCountriesData.countries?.map((country: any, index: number) => {
                         // Color palette for top 5 countries
                         const colors = [
